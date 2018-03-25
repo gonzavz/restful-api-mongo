8b380de5af643bc989ecb3cf96652bb501643fea
gonzaCOG <valdezg@gmail.com>
2018-03-25 00:30:58 -0300

    Moved app config and setup to from index.js to /src/app.js Added dev-dependencies:   - mocha   - supertest Created first test for testing setup :)

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const logger = require('./src/utils/logger');
+const app = require('./src/app');
 
 const port = process.env.PORT || 3000;
-
-const app = express();
-
-// Configure body parser
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-// Add morgan midleware combined with winston for http request logging.
-app.use(morgan('combined', {stream: logger.stream}));
-
-app.use('/', (req, res) => res.json('restful api v1.0.0'));
 
 // start the server
 app.listen(port, () => {
