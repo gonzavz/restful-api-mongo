5caebf881bec81c168592f2affe5bf69daca277c
gonzaCOG <valdezg@gmail.com>
2018-03-25 17:09:11 -0300

    Added pending tests for User service:  - Validate request body.  - Validate user model -> avatar is url.  - Validate create new user.

--- test/users.js
+++ test/users.js
@@ -8,12 +8,22 @@ const validUser = {
   avatar: 'http://someurl.com/image.png',
 };
 
+const invalidAvatar = {
+  name: 'pepe',
+  avatar: 'httpp://not_an_url.com',
+};
+
 const validHeaders = {
   'Authorization': process.env.AUTHORIZATION_TOKEN,
 };
 
 const invalidHeaders = {
   'Authorization': 'NOT_A_VALID_TOKEN',
+};
+
+// POST /users expect { name: required, avatar: optional}
+const invalidBodyNoName = {
+  'avatar': 'pepe',
 };
 
 logger.info(validHeaders);
@@ -39,6 +49,27 @@ describe('User Service', () => {
         .set(invalidHeaders)
         .expect('Content-Type', /json/)
         .expect(401, done);
+    });
+    it('Should not allow an invalid body', (done) => {
+      request(server)
+        .post('/users')
+        .send(invalidBodyNoName)
+        .set(validHeaders)
+        .expect(400, done);
+    });
+    it('Should validate avatar is a valid url', (done) => {
+      request(server)
+        .post('/users')
+        .send(invalidAvatar)
+        .set(validHeaders)
+        .expect(422, done);
+    });
+    it('Should create a new User', (done) => {
+      request(server)
+        .post('/users')
+        .send(validUser)
+        .set(validHeaders)
+        .expect(200, done);
     });
   });
 });
